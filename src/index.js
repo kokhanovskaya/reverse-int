aea123e67dab6f4b4ea49d059b91288a689014fe
Anastasiia <101743252+kokhanovskaya@users.noreply.github.com>
2024-08-10 18:39:15 +0400

    feat

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let strN = Math.abs(n).toString();
+
+  let reversedStr = '';
+  for (let i = strN.length - 1; i >= 0; i--) {
+    reversedStr += strN[i];
+    }
+  return reversedStr
 }
